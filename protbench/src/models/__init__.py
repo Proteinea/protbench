7284afda37b90d391ac6aa7aa2a372008b4598c0
Mohamed Elshaffei <62253044+mo-shaffei@users.noreply.github.com>
2023-06-11 05:54:43 +0300

    Add conda environment file

---
+++ protbench/src/models/__init__.py
@@ -0,0 +1,3 @@
+from protbench.src.models.pretrained import pretrained_models
+from protbench.src.models.downstream import downstream_models
+from protbench.src.models.model_registry import ModelRegistry
