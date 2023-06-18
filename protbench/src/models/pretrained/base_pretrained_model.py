a78e3e5e1cc8dd3dd44943bb0711c05e2bce7129
mo-shaffei <62253044+mo-shaffei@users.noreply.github.com>
2023-06-19 00:00:51 +0300

    Remove freeze_model() It's not needed since child classes could simply wrap embed_sequences() with torch.inference_mode() decorator

--- protbench/src/models/pretrained/base_pretrained_model.py
+++ protbench/src/models/pretrained/base_pretrained_model.py
@@ -9,11 +9,6 @@ class BasePretrainedModel(abc.ABC):
     Base class for pretrained models.
     """
 
-    def freeze_model(self, model):
-        model.eval()
-        for param in model.parameters():
-            param.requires_grad = False
-
     @abc.abstractmethod
     def embed_sequences(
         self, sequences: List[str], device: torch.device
